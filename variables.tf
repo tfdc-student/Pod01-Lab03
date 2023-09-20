8c24afa3da0c4a468eaa4cbaa99067b426757f5e
tfdc-student <101262372+tfdc-student@users.noreply.github.com>
2023-09-20 11:03:55 +0200

    Added variables.tf to lab 3

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "apikey" {
+  type = string
+  description = "Intersight KeyID (public key)"
+}
+
+variable "secretkey" {
+  type = string
+  description = "Intersight Secret Key (private key)"
+}
+
+variable "endpoint" {
+  type = string
+  description = "URL to Intersight"
+}
